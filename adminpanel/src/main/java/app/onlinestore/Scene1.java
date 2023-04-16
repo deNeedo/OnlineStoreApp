c01be43c2c4d252579bb7f796dfbc5eed54220c3
PilchS <stanislawpilch2002@gmail.com>
2023-04-16 14:42:33 +0200

    Added login functionality for admin

---
+++ adminpanel/src/main/java/app/onlinestore/Scene1.java
@@ -0,0 +1,90 @@
+package app.onlinestore;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.*;
+
+public class Scene1
+{
+
+    @FXML
+    TextField txtButton;
+
+    @FXML
+    TextField passButton;
+
+    @FXML
+    AnchorPane scenePane;
+
+    @FXML
+    Label errMess;
+
+
+    public Stage stage;
+    public Scene scene;
+    public Parent root;
+
+    public void login(ActionEvent event) throws Exception
+    {
+        // Create client socket
+        Socket socket = new Socket("localhost", 888);
+        // to send data to the server
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        // to read data coming from the server
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String username = txtButton.getText();
+        String pass = passButton.getText();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene2.fxml"));
+        root = loader.load();
+
+        Scene2 scene2 = loader.getController();
+        scene2.displayName(username);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        if(username.isEmpty())
+        {
+            errMess.setText("Write your username");
+            dos.close();
+            br.close();
+            socket.close();
+        }
+        else
+        {
+            if(pass.isEmpty())
+            {
+                errMess.setText("Write your password");
+                dos.close();
+                br.close();
+                socket.close();
+            }
+            else
+            {
+                dos.writeBytes(username + " " + pass + "\n");
+                if (br.readLine().equals("one"))
+                {
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+                else
+                {
+                    errMess.setText("Wrong login/password");
+                    dos.close();
+                    br.close();
+                    socket.close();
+                }
+            }
+        }
+    }
+}

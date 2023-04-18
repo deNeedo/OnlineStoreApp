16f60e1de89655035713a684475d03ce3813b57e
PilchS <stanislawpilch2002@gmail.com>
2023-04-18 18:27:32 +0200

    added logout button + fixed tab

--- adminpanel/src/main/java/app/onlinestore/Scene2.java
+++ adminpanel/src/main/java/app/onlinestore/Scene2.java
@@ -1,11 +1,13 @@
 package app.onlinestore;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Node;
 
 import java.io.*;
 import java.net.*;
@@ -24,6 +26,13 @@ public class Scene2
 
     public void logout(ActionEvent event) throws Exception
     {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("scene1.fxml"));
+            root = loader.load();
+            Scene1 Scene1 = loader.getController();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         dos.close();
         br.close();
         socket.close();

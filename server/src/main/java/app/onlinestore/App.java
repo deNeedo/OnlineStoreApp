d563f80dfb08c66b6ee624ff5a39149ae5390c91
deNeedo <jkudrys@protonmail.com>
2023-04-20 01:03:27 +0200

    fixed webserver

---
+++ server/src/main/java/app/onlinestore/App.java
@@ -0,0 +1,75 @@
+package app.onlinestore;
+
+// import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.glassfish.tyrus.server.Server;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class App
+{
+    protected final static String postgrespass = getProperty();
+    private static String getProperty()
+    {
+        Properties prop = new Properties();
+        String fileName = "postgres.config";
+        try (FileInputStream fis = new FileInputStream(fileName)) {prop.load(fis);}
+        catch (Exception e) {e.printStackTrace(); return null;}
+        return prop.getProperty("postgres.pass");
+    }
+    public static void main(String[] args)
+    {
+        Server server = new Server("localhost", 80, "/app", AppServerEndpoint.class);
+        try {server.start(); while (true) {}} // for now to make the server run in infinite loop
+        catch (Exception e) {throw new RuntimeException(e);}
+        finally {server.stop();}
+    }
+    public static String admin_login(String data) throws Exception
+    {
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=" + App.postgrespass;
+        dataSource.setUrl(url);
+		Connection conn = dataSource.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("select * from onlinestore.users where login like '"+ data.split(" ")[1] +"' and password like '" + data.split(" ")[2] + "' and type like 'administrator'" );
+		ResultSet rs = stmt.executeQuery();
+        String message = "not found";
+		while (rs.next()) {message = "found";}
+        return message;
+    }
+    public static String client_login(String data) throws Exception
+    {
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=" + App.postgrespass;
+        dataSource.setUrl(url);
+		Connection conn = dataSource.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("select * from onlinestore.users where login like '"+ data.split(" ")[1] +"' and password like '" + data.split(" ")[2] + "' and type like 'customer'" );
+		ResultSet rs = stmt.executeQuery();
+        String message = "not found";
+		while (rs.next()) {message = "found";}
+        return message;
+    }
+    public static String client_register(String data) throws Exception
+    {
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=" + App.postgrespass;
+        dataSource.setUrl(url);
+		Connection conn = dataSource.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("select * from onlinestore.users where login like '"+ data.split(" ")[1] + "'");
+		ResultSet rs = stmt.executeQuery();
+        String message = "not found";
+		while (rs.next()) {message = "found";}
+        if (message.contains("not found"))
+        {
+            stmt = conn.prepareStatement("insert into onlinestore.users (user_id, login, password, first_name, last_name, phone_number) values (?, ?, ?, ?, ?, ?)");
+            stmt.setInt(1, 100); stmt.setString(2, data.split(" ")[1]);
+            stmt.setString(3, data.split(" ")[2]); stmt.setString(4, data.split(" ")[3]);
+            stmt.setString(5, data.split(" ")[4]); stmt.setString(6, data.split(" ")[5]);
+            stmt.executeUpdate();
+        }
+        return message;
+    }
+}
---
+++ server/src/main/java/app/onlinestore/AppServerEndpoint.java
@@ -0,0 +1,32 @@
+package app.onlinestore;
+
+import java.util.logging.Logger;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.CloseReason.CloseCodes;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/onlinestore")
+public class AppServerEndpoint
+{
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    @OnOpen
+    public void onOpen(Session session) {logger.info("Connection " + session.getId() + " established.");}
+    @OnMessage
+    public String onMessage(Session session, String message) throws Exception
+    {
+        logger.info(message);
+        if (message.contains("admin-login-try")) {return App.admin_login(message);}
+        else if (message.contains("client-login-try")) {return App.client_login(message);}
+        else if (message.contains("client-register-try")) {return App.client_register(message);}
+        else {session.close(new CloseReason(CloseCodes.NORMAL_CLOSURE, "")); return null;}
+    }
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason)
+    {
+        logger.info("Connection " + session.getId() + " closed.");
+    }
+}

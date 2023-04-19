b0f49ecbbf2c77b1d2eb7428632f451e79cc7ed1
PilchS <stanislawpilch2002@gmail.com>
2023-04-19 20:32:19 +0200

    deleted comment

--- adminpanel/src/main/java/app/onlinestore/AdminPanel.java
+++ adminpanel/src/main/java/app/onlinestore/AdminPanel.java
@@ -13,7 +13,7 @@ public class AdminPanel extends Application
     {
         try
         {
-            Parent root = FXMLLoader.load(getClass().getResource("start.fxml")); //shows first scene
+            Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();

4cb45ec7f31ed9e4d162b444adebaa63d901bdbf
PilchS <stanislawpilch2002@gmail.com>
2023-04-22 18:42:47 +0200

    Added icon and title

--- adminpanel/src/main/java/app/onlinestore/AdminPanel.java
+++ adminpanel/src/main/java/app/onlinestore/AdminPanel.java
@@ -2,8 +2,10 @@ package app.onlinestore;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AdminPanel extends Application
@@ -15,6 +17,12 @@ public class AdminPanel extends Application
         {
             Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
             Scene scene = new Scene(root);
+
+            Image icon = new Image("icon.png");
+            stage.getIcons().add(icon);
+
+            stage.setTitle("Admin Panel");
+
             stage.setScene(scene);
             stage.show();       
         }

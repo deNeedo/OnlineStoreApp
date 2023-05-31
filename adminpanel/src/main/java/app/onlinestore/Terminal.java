54405c77a6b9680e2de644f8850eb48412b28f9f
deNeedo <jkudrys@protonmail.com>
2023-05-31 22:45:01 +0200

    Added query sending function

--- adminpanel/src/main/java/app/onlinestore/Terminal.java
+++ adminpanel/src/main/java/app/onlinestore/Terminal.java
@@ -4,53 +4,57 @@ import javax.websocket.Session;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+// import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.concurrent.TimeUnit;
+
 public class Terminal
 {
-    @FXML
-    Label result_field;
+    @FXML TextArea result_field;
+    @FXML TextField query_field;
+    @FXML Button execute_button;
+    @FXML Button leave_button;
 
-    @FXML
-    TextField query_field;
-
-    @FXML
-    Button execute_button;
-
-    @FXML
-    Button leave_button;
-
+    // private FXMLLoader loader = new FXMLLoader();
     public Scene2 previous;
     public Session session;
+    public String message;
     public Parent base;
     public Stage stage;
     public Scene scene;
 
-    public void leave(ActionEvent event) throws Exception{
-        base = FXMLLoader.load(getClass().getResource("scene2.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(base);
-        stage.setScene(scene);
-        stage.show();
+    public Session getSession() {return this.session;}
+    public void setPrevious(Scene2 previous) {this.previous = previous;}
+
+    public void leave(ActionEvent event) throws Exception {
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.stage.setResizable(false);
+        this.stage.setScene(this.previous.previous.scene); this.stage.show();
+        this.result_field.setText("");
     }
 
-    public void psql(ActionEvent event) throws Exception{
-        //writing a query
-        String query = query_field.getText();
+    // public void psql(ActionEvent event) throws Exception {
+    //     // writing a query
+    // }
+
+    public void execute(ActionEvent event) throws Exception {
+        this.result_field.setText("");
+        if (this.session == null) {this.session = this.previous.getSession();}
+        // execute the query
+        String query = this.query_field.getText();
+        this.session.getBasicRemote().sendText("admin-query:" + query);
+        while (this.message == null) {TimeUnit.MILLISECONDS.sleep(1);}
+        this.result_field.setText(this.message);
     }
 
-    public void execute(ActionEvent event) throws Exception{
-        //execute the query
-    }
-
-    public void show(ActionEvent event) throws Exception{
-        //showing a result 
+    public void show(ActionEvent event) throws Exception {
+        // showing a result 
     }
 }

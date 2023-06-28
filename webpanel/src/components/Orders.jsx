81555d0c21dad647abf0cdb3531d04c2bca40153
Jeremi Sadkowski <jsadkowski128@gmail.com>
2023-06-28 23:14:06 +0200

    Order array not working correctly

--- webpanel/src/components/Orders.jsx
+++ webpanel/src/components/Orders.jsx
@@ -1,7 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate, useLocation} from 'react-router-dom';
 import NotificationsSystem, {atalhoTheme, useNotifications} from 'reapop';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import TermsCss from './css/Terms.module.css';
 import Header from './Header';
 import Footer from './Footer';
@@ -13,6 +19,18 @@ export function Orders() {
     const [auth, setAuth] = useState(location.state.auth);
     const [orders, setOrders] = useState([]);
     const order_arr = [];
+
+    function createData(email, item, quantity, data, time, price) {
+        return { email, item, quantity, data, time, price };
+      }
+      
+    //   const rows = [
+    //     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    //     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    //     createData('Eclair', 262, 16.0, 24, 6.0),
+    //     createData('Cupcake', 305, 3.7, 67, 4.3),
+    //     createData('Gingerbread', 356, 16.0, 49, 3.9),
+    //   ];
 
     const get_orders = () => {
         let socket = new WebSocket('ws://localhost:80/veggiestore'); let message;
@@ -30,7 +48,48 @@ export function Orders() {
         <div onLoad={get_orders} className={TermsCss['wrapper']}>
             <Header props={{setLang, lang, setAuth, auth}} />
                 <button onClick={get_orders}> REFRESH </button>
-                {orders.map((order) => (order))}
+                {/* {orders.map((order) => (order))} */}
+
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Email</TableCell>
+                    <TableCell align="right">Item</TableCell>
+                    <TableCell align="right">Quantity</TableCell>
+                    <TableCell align="right">Data</TableCell>
+                    <TableCell align="right">Time</TableCell>
+                    <TableCell align="right">Price</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {orders.map((order) => ( <TableRow>
+                <TableCell align="right">{order.split(" ")[0]}</TableCell>
+                <TableCell align="right">{order.split(" ")[1]}</TableCell>
+                <TableCell align="right">{order.split(" ")[2]}</TableCell>
+                <TableCell align="right">{order.split(" ")[3]}</TableCell>
+                <TableCell align="right">{order.split(" ")[4]}</TableCell>
+                <TableCell align="right">{order.split(" ")[5]}</TableCell>
+                </TableRow>
+                ))}
+                {/* {rows.map((row) => (
+                    <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell component="th" scope="row">
+                        {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                ))} */}
+                </TableBody>
+            </Table>
+            </TableContainer>
+
             <Footer/>
         </div>
     )
